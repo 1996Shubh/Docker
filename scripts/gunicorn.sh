29171da79625979fc1d5b792589ba9bf3af89833
1996Shubh <modishubh29@gmail.com>
2020-03-30 18:55:14 +0530

    Added

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp /home/ubuntu/Chatapp_project/nginx/gunicorn.services /etc/systemd/system/
+sudo cp /home/ubuntu/Chatapp_project/nginx/gunicorn.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
