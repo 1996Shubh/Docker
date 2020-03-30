fc646983caa8f6051642a5ee743791a2e976ea0e
1996Shubh <60534943+1996Shubh@users.noreply.github.com>
2020-03-30 19:08:23 +0530

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -2,7 +2,7 @@
 
 sudo unlink /etc/nginx/sites-enabled/*
 sudo cp /home/ubuntu/Chatapp_project/nginx/fundoo /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/chatapp /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/fundoo /etc/nginx/sites-enabled
 sudo nginx -t
 sudo rm /etc/nginx/sites-enabled/default
 sudo service nginx restart
