86fbd8b8deb2192539b1d801a65ee07a5a61a431
1996Shubh <60534943+1996Shubh@users.noreply.github.com>
2020-03-30 19:49:04 +0530

    Update install_dependancies.sh

--- scripts/install_dependancies.sh
+++ scripts/install_dependancies.sh
@@ -3,9 +3,9 @@ sudo apt-get update
 sudo apt-get install python3-pip python3-dev nginx git -y
 sudo apt-get update
 pip3 install virtualenv
-cd /home/ubuntu/Chatapp_project/
+cd /home/ubuntu/Chatapp_project/Chatapplication
 virtualenv --python=python3 venv
-source /home/ubuntu/Chatapp_project/venv/bin/activate
+source /home/ubuntu/Chatapp_project/Chatapplication/venv/bin/activate
 cd /home/ubuntu/Chatapp_project/Chatapplication/chatapp
 pip3 install -r requirements.txt
 pip3 install django bcrypt django-extensions
